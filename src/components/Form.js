2c52da1ec091b6be3e7dc6aa3858d8ff334993ca
[Georgi Karchev] <georgikarcev@gmail.com>
2024-01-30 20:51:58 -0500

    Adding functions handleChange and handleSubmit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,33 @@
+import {useState, useEffect} from "react";
 export default function Form(props){
-    // The component must return some JSX
+   //state to hold the data of our form
+   const [formData, setFormData] = useState({
+    searchterm: "",
+   });
+
+   //handleChange - updates formData when we type into form
+   const handleChange = (event) => {
+    //Use the event object to detect key, and value to update
+    setFormData({...formData, [event.target.name]: event.target.value});
+   };
+
+   const handleSubmit = (event) => {
+    //Prevent Page from refreshing on form submission
+    event.preventDefault();
+    //pass the search term to moviesearch prop, which is App's getMovie function
+    props.moviesearch(formData.searchterm);
+   };
+
     return (
       <div>
         <h1>The Form Component</h1>;
-        <form>
-            <input type ="text" />
+        <form onSubmit={handleSubmit}>
+            <input
+             type ="text" 
+             name ="searchterm"
+             onChange = {handleChange}
+             value = {formData.searchterm}
+            />
             <input type ="submit" value="submit"/>
         </form>
       </div>
